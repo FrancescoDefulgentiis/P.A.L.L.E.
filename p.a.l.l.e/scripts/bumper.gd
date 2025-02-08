a9d3013b23ddf7472a263c19ddecb0cf659f170e
francescoDefulgentiis <fran.cesco.defulgentiis@gmail.com>
2025-02-08 18:32:01 +0100

    debugging

--- p.a.l.l.e/scripts/bumper.gd
+++ p.a.l.l.e/scripts/bumper.gd
@@ -11,23 +11,25 @@ func _process(delta: float) -> void:
 	pass
 
 
-const SPEED_MULTIPLIER = 10 # Increase speed by 50%
+const SPEED_MULTIPLIER = 2 
 
 func velocity_evaluate(Velocity : Vector3 , Position: Vector3)-> Vector3:
 	print("evaluating_trajegtory")
-	var new_velocity : Vector3
-	var radius=$CollisionShape3D.shape.radius
-	print("ball position: " ,Position)
-	var angle=atan2(Position.y,Position.x)
-	angle=(angle*180)/3.14
-	Velocity.x=(Velocity.x*cos(angle)+Velocity.y*cos(angle))*SPEED_MULTIPLIER
-	Velocity.y=(Velocity.x*sin(angle)+Velocity.y*sin(angle))
-	Velocity.z=0
-	return Velocity
+	var New_velocity : Vector3
+	print("ball position x", Position.x)
+	print("ball position y", Position.y)
+	print("ball position z", Position.z)
+	
+	var Angle=atan2(Position.y,Position.z)
+	print("collision angle: " ,rad_to_deg(Angle))
+	var New_velocity_x = (Velocity.x * cos(Angle) - Velocity.y * sin(Angle)) * SPEED_MULTIPLIER
+	var New_velocity_y = (Velocity.x * sin(Angle) + Velocity.y * cos(Angle)) * SPEED_MULTIPLIER
+
+	return Vector3(New_velocity_x, New_velocity_y, 0)
 	
 func _on_body_entered(body):
 		print("body entered")
-		var normal = to_local(body.global_position) # Adjust the normal based on collision direction
-		var incoming_velocity = body.linear_velocity
-		var new_velocity=velocity_evaluate(incoming_velocity,normal)
-		body.linear_velocity=new_velocity
+		var Position = to_local(body.global_position) 
+		var Incoming_velocity = body.linear_velocity
+		var New_velocity=velocity_evaluate(Incoming_velocity,Position)
+		body.linear_velocity=New_velocity
