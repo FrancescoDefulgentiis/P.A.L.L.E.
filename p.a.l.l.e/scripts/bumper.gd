93fd700e9c6d74724c74ec32b5fee9c74d9ab93f
ghosciro <dennislocatelli2001@gmail.com>
2025-02-08 19:31:07 +0100

    dev den

--- p.a.l.l.e/scripts/bumper.gd
+++ p.a.l.l.e/scripts/bumper.gd
@@ -11,6 +11,7 @@ func _process(delta: float) -> void:
 	pass
 
 
+<<<<<<< Updated upstream
 const SPEED_MULTIPLIER = 10 # Increase speed by 50%
 
 func velocity_evaluate(Velocity : Vector3 , Position: Vector3)-> Vector3:
@@ -31,3 +32,25 @@ func _on_body_entered(body):
 		var incoming_velocity = body.linear_velocity
 		var new_velocity=velocity_evaluate(incoming_velocity,normal)
 		body.linear_velocity=new_velocity
+=======
+const SPEED_MULTIPLIER = 1
+
+func velocity_evaluate(Velocity : Vector3 , Position: Vector3)-> Vector3:
+	print("evaluating_trajegtory")
+	var New_velocity : Vector3
+	var Angle=atan2(Position.y,Position.x)
+	print("collision angle: " ,rad_to_deg(Angle))
+	print("starting Velocity: ",Velocity)
+	var New_velocity_x = ((Velocity.x * cos(Angle) - Velocity.y * sin(Angle)) * SPEED_MULTIPLIER)*sign(cos(Angle))
+	var New_velocity_y = ((Velocity.x * sin(Angle) + Velocity.y * cos(Angle)) * SPEED_MULTIPLIER)*sign(sin(Angle))
+	print(New_velocity_x," ",New_velocity_y)
+	return Vector3(New_velocity_x, New_velocity_y, 0)
+	
+func _on_body_entered(body):
+		print("body entered")
+		
+		var Position = body.global_position-global_position
+		var Incoming_velocity = body.linear_velocity
+		var New_velocity=velocity_evaluate(Incoming_velocity,Position)
+		body.linear_velocity=New_velocity
+>>>>>>> Stashed changes
