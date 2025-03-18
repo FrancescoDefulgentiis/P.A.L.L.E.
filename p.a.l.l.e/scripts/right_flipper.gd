47dc33e0b6cc8b039b0f7e184b01ad603f56c5dd
francescoDefulgentiis <fran.cesco.defulgentiis@gmail.com>
2025-03-18 01:31:15 +0100

    improved flipper physics, added walls interactions, not really a good point

--- p.a.l.l.e/scripts/right_flipper.gd
+++ p.a.l.l.e/scripts/right_flipper.gd
@@ -1,14 +1,23 @@
 extends RigidBody2D
 
+@export var flipper_strength: float = 80000  # Force applied to the flipper
+@export var key_bind: String = "R2"      
 
-func _ready() -> void:
-	pass 
+var is_flipping: bool = false
 
+func _ready():
+	physics_material_override = PhysicsMaterial.new()
+	physics_material_override.friction = 1.0
+	physics_material_override.bounce = 0.0
 
 func _process(_delta):
-	
-	if Input.is_action_pressed("R2"):
-		angular_velocity = 10
+	if Input.is_action_just_pressed(key_bind):
+		is_flipping = true
+	elif Input.is_action_just_released(key_bind):
+		is_flipping = false
+
+func _physics_process(delta):
+	if is_flipping:
+		apply_torque(flipper_strength)
 	else:
-		angular_velocity = 0
-	rotation = clamp(rotation, deg_to_rad(0), deg_to_rad(60))
+		apply_torque(0)  
